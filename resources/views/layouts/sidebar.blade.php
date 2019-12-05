f77ad1dab947564a56195301285bfa21927ac63b
ystmrdk <rattyasta1@gmail.com>
2019-12-05 13:21:47 +0700

    layout modified

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,23 @@
+<div class="sidebar-wrapper">
+    <ul class="nav">
+      <li class="{{ (request()->is('/')) ? 'active' : '' }}">
+        <a href="/">
+          <i class="nc-icon nc-circle-10"></i>
+          <p>Dashboard</p>
+        </a>
+      </li>
+      <li class="{{ (request()->is('/tambah*')) ? 'active' : '' }}">
+        <a href="/tambah">
+          <i class="nc-icon nc-simple-add"></i>
+          <p>Tambah</p>
+        </a>
+      </li>
+      <li class="{{ (request()->is('/edit')) ? 'active' : '' }}">
+        <a href="/edit">
+          <i class="nc-icon nc-ruler-pencil"></i>
+          <p>Edit</p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,64 @@
+@extends('admin')
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title"> Data Siswa</h4>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class=" text-primary">
+                            <th>
+                                Name
+                            </th>
+                            <th>
+                                Phone
+                            </th>
+                            <th class="text-right">
+                                Email
+                            </th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    Dakota Rice
+                                </td>
+                                <td>
+                                    Niger
+                                </td>
+                                <td class="text-right">
+                                    $36,738
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Dakota Rice
+                                </td>
+                                <td>
+                                    Niger
+                                </td>
+                                <td class="text-right">
+                                    $36,738
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Dakota Rice
+                                </td>
+                                <td>
+                                    Niger
+                                </td>
+                                <td class="text-right">
+                                    $36,738
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
